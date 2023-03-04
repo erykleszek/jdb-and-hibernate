aebcd7cf4719d72979288a687efd5188765e2be0
Lesio <e.leszek.one@gmail.com>
2023-03-04 11:27:30 +0100

    [Change] = more output to console

--- src/main/java/pl/sda/orange2/jah/Main.java
+++ src/main/java/pl/sda/orange2/jah/Main.java
@@ -3,5 +3,6 @@ package pl.sda.orange2.jah;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Works...");
+        System.out.println("JDBC and Hiberante - love");
     }
 }
