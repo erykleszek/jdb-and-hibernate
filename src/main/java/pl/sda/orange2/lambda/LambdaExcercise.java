3f00c1c6fcddbf0a8692c0ea4c66f340da814b3c
Lesio <e.leszek.one@gmail.com>
2023-03-04 13:32:05 +0100

    [add] = interface with implementation

--- src/main/java/pl/sda/orange2/lambda/LambdaExcercise.java
+++ src/main/java/pl/sda/orange2/lambda/LambdaExcercise.java
@@ -1,6 +1,14 @@
 package pl.sda.orange2.lambda;
 
 public class LambdaExcercise {
+    public static void main(String[] args) {
 
 
+        BoysDontCry boy = new chlopakinieplacza();
+        boy.silnoreki();
+        // boy.sing();
+        chlopakinieplacza boy2 = new chlopakinieplacza();
+        boy2.sing();
+
+    }
 }
---
+++ src/main/java/pl/sda/orange2/lambda/chlopakinieplacza.java
@@ -0,0 +1,13 @@
+package pl.sda.orange2.lambda;
+
+public class chlopakinieplacza implements BoysDontCry{
+
+    @Override
+    public void silnoreki() {
+        System.out.println("Jestem silnoręki"); }
+
+        public void sing() {
+            System.out.println("sing");
+        }
+    }
+
