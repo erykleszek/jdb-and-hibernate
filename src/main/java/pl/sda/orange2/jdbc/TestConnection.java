82f0ae10c1b574a2c9acd0d02b0dd5c28c1e568d
Lesio <e.leszek.one@gmail.com>
2023-03-05 16:01:36 +0100

    [change] - Got connection

--- src/main/java/pl/sda/orange2/jdbc/TestConnection.java
+++ src/main/java/pl/sda/orange2/jdbc/TestConnection.java
@@ -2,19 +2,34 @@ package pl.sda.orange2.jdbc;
 
 import pl.sda.orange2.jdbc.config.H2Config;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class TestConnection {
     public static void main(String[] args) {
+        String query = """
+                SELECT ID, NAME
+                FROM TEST
+                """;
+
         try {
-          var h2Connection = DriverManager.getConnection(H2Config.DB_URL,
+//            Connection h2Connection = DriverManager.getConnection(H2Config.DB_URL,
+//                    H2Config.USER,
+//                    H2Config.PASSWORD);
+            var h2Connection = DriverManager.getConnection(H2Config.DB_URL,
                     H2Config.USER,
                     H2Config.PASSWORD);
-            System.out.println("got connection " + (h2Connection != null));
-        } catch (SQLException exc) {
-            System.out.println("got exception: " +exc);
+            System.out.println("got connection: " + (h2Connection != null));
 
+            Statement queryStatement = h2Connection.createStatement();
+            ResultSet queryResult = queryStatement.executeQuery(query);
+            queryResult.next();
+            while (queryResult.next()) {
+                System.out.println("id: " + queryResult.getInt(1));
+                System.out.println("name: " + queryResult.getString(2));
+            }
+        } catch (SQLException exc) {
+            System.out.println("got exception: " + exc);
+            exc.printStackTrace();
         }
     }
 }
